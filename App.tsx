b2583da73dcd35d51d8460098459aeb65d3f62c8
unknown <mehankjain1670@gmail.com>
2020-06-13 13:09:04 +0530

    socket test

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Routes } from "./Routes";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
+import io from "socket.io-client";
 
 const loadAssets = () => {
+  const socket = io("http://192.168.29.71:4000");
   return Font.loadAsync({
     "OpenSans-SemiBold": require("./assets/fonts/OpenSans-SemiBold.ttf"),
     "Microsoft-YaHei-Bold": require("./assets/fonts/Microsoft-YaHei-Bold.ttf"),
